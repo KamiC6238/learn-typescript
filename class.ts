3c96f7aba34fa666242819ef983bddca352aa6bd
danzzzz <823913645@qq.com>
2020-04-13 19:40:17 +0800

    类的继承和重写

---
+++ class.ts
@@ -0,0 +1,27 @@
+class Animal {
+  name: string
+
+  constructor (name: string) {
+    this.name = name
+  }
+
+  run () {
+    return `${this.name} is running`
+  }
+}
+
+class Cat extends Animal {
+  // 在子类使用 constructor 时，必须调用super方法
+  constructor (name) {
+    super(name)
+    console.log(this.name) 
+  }
+  
+  // 重写父类的 run 方法
+  run () {
+    return 'Meow, ' + super.run()
+  }
+}
+
+const maomao = new Cat('maomao')
+console.log(maomao.run())
